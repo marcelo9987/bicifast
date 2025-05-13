2163aaaa9c0a4e64a46c30d4c2020270ccfaa023
marcelo9987 <26576174+marcelo9987@users.noreply.github.com>
2025-05-13 17:57:29 +0200

    Creo archivos basicos + esqueleto mínimo

---
+++ App/src/main/java/aplicacion/FachadaAplicacion.java
@@ -0,0 +1,13 @@
+package aplicacion;
+
+import basededatos.FachadaBaseDatos;
+
+public class FachadaAplicacion {
+
+    public static void main(String[] args)
+    {
+        FachadaBaseDatos fachada = new FachadaBaseDatos();
+        fachada._funciona();
+
+    }
+}
---
+++ App/src/main/java/basededatos/FachadaBaseDatos.java
@@ -0,0 +1,58 @@
+package basededatos;
+
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Properties;
+
+
+public class FachadaBaseDatos {
+    private java.sql.Connection conexion;
+
+    public FachadaBaseDatos (){
+
+        Properties configuracion = new Properties();
+        FileInputStream arqConfiguracion;
+
+        try {
+            arqConfiguracion = new FileInputStream("App/baseDatos.properties");
+            configuracion.load(arqConfiguracion);
+            arqConfiguracion.close();
+
+            Properties usuario = new Properties();
+
+
+            String gestor = configuracion.getProperty("gestor");
+
+            usuario.setProperty("user", configuracion.getProperty("usuario"));
+            usuario.setProperty("password", configuracion.getProperty("clave"));
+            this.conexion=java.sql.DriverManager.getConnection("jdbc:"+gestor+"://"+
+                            configuracion.getProperty("servidor")+":"+
+                            configuracion.getProperty("puerto")+"/"+
+                            configuracion.getProperty("baseDatos"),
+                    usuario);
+
+
+        } catch (SQLException | IOException f){
+            System.out.println(f.getMessage());
+        }
+
+
+    }
+
+    public void _funciona() {
+        // Prueba de conexión
+        try {
+            if (conexion != null && !conexion.isClosed()) {
+                System.out.println("Conexión exitosa a la base de datos.");
+            }
+            else {
+                System.out.println("No se pudo establecer la conexión a la base de datos.");
+            }
+        } catch (SQLException e) {
+            System.out.println("Error al verificar la conexión: " + e.getMessage());
+        }
+    }
+
+}
